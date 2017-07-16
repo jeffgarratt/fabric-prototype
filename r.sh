7504aa5cf5a77307279c6e54e8cf6a241858de9a
Jeff Garratt <garratt.jeff@gmail.com>
2017-07-16 13:18:09 -0400

    Initial commit

---
+++ r.sh
@@ -0,0 +1,5 @@
+docker unpause $(docker ps -qa)
+docker kill $(docker ps -qa)
+docker rm $(docker ps -qa)
+docker network rm $(docker network ls -q)
+docker rmi $(docker images | grep "\-vp[0-9]*\-" | tr -s ' ' | cut -d ' ' -f 1 | less)
